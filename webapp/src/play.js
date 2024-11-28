241197f541b625b2b2426a74323a4359946f9992
Alexey <12871810+alexeyhimself@users.noreply.github.com>
2024-11-28 07:27:12 +0300

    Update play.js

--- webapp/src/play.js
+++ webapp/src/play.js
@@ -134,8 +134,9 @@ async function create_drawings_list() {
 
   //const stats = await get_players_stats();
   //html_past = `<p>Общее количество участников на данный момент ${stats.total}, которые все вместе за все время выиграли ${times(stats.total_winners)}. Вчера из ${stats.yesterday} участников ${stats.yesterday_winners} выиграли. Сегодня на данный момент участвуют&nbsp;${stats.today}.</p>` + html_past;
-  if (uid != 1730926893589)
-    html_past = '<p class="transfer-data-hint">❇️ Если вы участвовали в розыгрыше 15-16 ноября из браузера, то вы можете перенести всю свою историю и настройки сюда. Для этого, откройте <a href="https://adwesome.github.io/draws/webapp/index.html?v=10" target="_blank">adwesome.github.io/draws/webapp?v=1</a> в браузере, в котором вы участвовали, и следуйте инструкции.</p>' + html_past;
+  //if (uid != 1730926893589)
+  
+  html_past = '<p class="transfer-data-hint">❇️ Если вы участвовали в розыгрыше 15-16 ноября из браузера, то вы можете перенести всю свою историю и настройки сюда. Для этого, откройте <a href="https://adwesome.github.io/draws/webapp/index.html" target="_blank">https://adwesome.github.io/draws/webapp</a> в браузере, в котором вы участвовали, и следуйте инструкциям.</p>' + html_past;
 
 
   if (html_just)
@@ -162,7 +163,8 @@ async function create_drawings_list() {
     document.getElementById('now').innerHTML = html_now;
   if (html_past && past_counter)
     document.getElementById('past').innerHTML = html_past;
-
+  else if (html_past)
+    document.getElementById('past').innerHTML = document.getElementById('past').innerHTML + html_past;
   if (wins || lost || not)
     draw_chart(wins, lost, not);
 }
