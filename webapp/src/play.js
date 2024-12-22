5088ada780c9d1a46fe11cf5d923d584461c1303
Alexey <12871810+alexeyhimself@users.noreply.github.com>
2024-12-22 11:27:05 +0300

    fixes

--- webapp/src/play.js
+++ webapp/src/play.js
@@ -39,7 +39,7 @@ async function create_drawings_list() {
       if (ch.demography.region == 10 && ch.demography.city == 11)
         init_orgs_poll();
 
-      if (ch.brands.length == 3 && ch.demography.city == 11)
+      if ([1,2,3,4,5].includes(ch.brands.length) && ch.demography.city == 11)
         document.getElementById('fill_data').innerHTML += '<div class="alert alert-primary" role="alert">Заполните, пожалуйста, форму в разделе "Бренды". Это анонимно и на данном этапе очень важно для будущего этого проекта. Спасибо!</div>';
 
       if (ch.demography.city != 11) {
