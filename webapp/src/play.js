2fb2b7cc87db3fd8192b691e3333130ad70c4b27
Alexey <12871810+alexeyhimself@users.noreply.github.com>
2024-12-30 21:59:07 +0300

    fixes

--- webapp/src/play.js
+++ webapp/src/play.js
@@ -561,7 +561,7 @@ async function play() {
     explain.style.transform = 'unset';
     explain.style.top = 'unset';
     explain.style.left = '-25px';
-    explain.innerHTML = '<video id="video" width="auto" height="auto" autoplay="autoplay" loop="loop" muted defaultMuted playsinline oncontextmenu="return false;" preload="auto">\
+    explain.innerHTML = '<video id="video" width="auto" height="auto" autoplay="autoplay" loop="loop" playsinline oncontextmenu="return false;" preload="auto">\
       <source src="https://www.dropbox.com/s/l48qpima7jge13i/shale.mp4?raw=1" type="video/mp4">\
       </video>';
     explain.style.display = 'block';
